a74bd0cd63089f31b58d0fd64644b7ed306ef061
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-02-21 20:19:07 +0900

    mysql 연동하기(App.tsx 클래스 형태로 변경) 확인 및 연동까지 완료

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const db = require('./config/db');
 
 app.get('/api/host', (req, res) => {
-  res.send('scs');
+  res.send({host: 'scs'});
 });
+
+app.get('/api/test', (req, res) => {
+  db.query("select * from test", (err, data) => {
+    if(!err) {
+      res.send(data);
+    } else {
+      console.log(err);
+      res.send(err);
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React, { useCallback, useState } from 'react';
+/* eslint-disable */
+import React, { Component } from 'react';
 import axios from 'axios';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h3>Welcome to SCS Blog! </h3>
-    </div>
-  );
-};
+class App extends Component<{}, {host: string, test: string}> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      host : '',
+      test: '',
+    }
+  }
+
+  componentDidMount() {
+    this._dbTest();
+  }
+  
+  _dbTest = async() => {
+    const res = await axios.get('/api/test');
+    console.log(res.data)
+  }
+
+
+  render() {
+    return(
+      <div className='App'>
+        <h3> Welcome to <u> {this.state.host} </u> Blog! </h3>
+      </div>
+    )
+  }
+}
 
 export default App;
+
+
