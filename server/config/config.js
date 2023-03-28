e2afc9b40936aeb6bb43cc3094cf695779d3f485
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-03-28 20:45:50 +0900

    yaml 적용

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "username": "tutorial_user",
     "password": process.env.MYSQL_PASSWORD,
     "database": "sample",
-    "host": "tutorial-db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com",
+    "host": "db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com",
     "port": "3306",
     "dialect": "mysql"
   },
@@ -13,7 +13,7 @@ module.exports = {
     "username": "tutorial_user",
     "password": process.env.MYSQL_PASSWORD,
     "database": "sample",
-    "host": "tutorial-db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com",
+    "host": "db-instance.clfzc3a1xomr.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
