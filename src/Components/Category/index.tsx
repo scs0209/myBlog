51244a7326aaa43a7daf7a599742aff91ef03868
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-08-24 20:23:48 +0900

    delete: CateogoryProvider 컴포넌트 삭제

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -1,4 +1,3 @@
-import { CategoryProvider } from 'contexts/categoryContext';
 import React, { VFC } from 'react';
 
 import CreateCategoryModal from '../../Components/onCreateCategoryModal';
@@ -12,22 +11,20 @@ interface Props {
 
 const Category: VFC<Props> = ({ showSidebar }) => {
   return (
-    <CategoryProvider>
-      <aside
-        id="logo-sidebar"
-        className={`fixed z-40 w-64 top-20 transition-transform ${
-          showSidebar ? 'left-2 translate-x-0 sm:translate-x-0' : '-translate-x-full sm:-left-2'
-        }`}
-        aria-label="Sidebar"
-      >
-        <Visitor />
-        <SideBar />
-        <div className="h-[calc(85vh-20rem)] px-3 pb-4 overflow-y-auto bg-gray-200 dark:bg-gray-800 border-gray-700 rounded-lg shadow-md">
-          <CategoryName />
-        </div>
-        <CreateCategoryModal />
-      </aside>
-    </CategoryProvider>
+    <aside
+      id="logo-sidebar"
+      className={`fixed z-40 w-64 top-20 transition-transform ${
+        showSidebar ? 'left-2 translate-x-0 sm:translate-x-0' : '-translate-x-full sm:-left-2'
+      }`}
+      aria-label="Sidebar"
+    >
+      <Visitor />
+      <SideBar />
+      <div className="h-[calc(85vh-20rem)] px-3 pb-4 overflow-y-auto bg-gray-200 dark:bg-gray-800 border-gray-700 rounded-lg shadow-md">
+        <CategoryName />
+      </div>
+      <CreateCategoryModal />
+    </aside>
   );
 };
 
