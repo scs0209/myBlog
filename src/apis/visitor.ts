c9171acbe464c85c1dc45a757ff77ecc727cc091
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-09-03 18:35:19 +0900

    fix: 첫 로딩 에러 해결 테스트(2)

--- src/apis/visitor.ts
+++ src/apis/visitor.ts
@@ -2,31 +2,23 @@ import { client } from 'apis';
 import { useQuery } from 'react-query';
 
 export const useTodayVisitors = () =>
-  useQuery(
-    'todayVisitors',
-    async () => {
+  useQuery('todayVisitors', async () => {
+    try {
       const { data } = await client.get('/api/visitors');
 
       return data;
-    },
-    {
-      onError: (error) => {
-        console.error('Error in useTodayVisitors:', error);
-      },
-    },
-  );
+    } catch (error) {
+      console.error('Error in useTodayVisitors:', error);
+    }
+  });
 
 export const useTotalVisitors = () =>
-  useQuery(
-    'totalVisitors',
-    async () => {
+  useQuery('totalVisitors', async () => {
+    try {
       const { data } = await client.get('/api/total-visitors');
 
       return data;
-    },
-    {
-      onError: (error) => {
-        console.error('Error in useTodayVisitors:', error);
-      },
-    },
-  );
+    } catch (error) {
+      console.error('Error in useTotalVisitors:', error);
+    }
+  });
